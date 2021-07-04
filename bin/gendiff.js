4997e9bccd09208cd7b24414f7771082cfb24fe0
erikaleie <bogachevanadia@yandex.ru>
2021-07-04 21:28:08 +0200

    Add tests and github actions

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,10 +6,10 @@ import genDiff from '../src/gendiff.js';
 program
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1') 
+  .version('0.0.1')
   .option('-f, --format [type]', 'output format')
   .action((path1, path2) => {
-    console.log(genDiff(path1, path2));      
+    console.log(genDiff(path1, path2));
   });
 
 program.parse();
