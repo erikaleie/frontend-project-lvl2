3615396aa664e54d9b6648f51873f9400419b27e
erikaleie <bogachevanadia@yandex.ru>
2021-07-08 17:50:49 +0200

    Add test and github actions

--- __tests__/gendiff.jest.js
+++ __tests__/gendiff.jest.js
@@ -3,10 +3,14 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import gendiff from '../index.js';
 
+console.log(import.meta.url);
+
 const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+console.log(getFixturePath('result_plain'));
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const result = readFile('result_plain');
--- src/gendiff.js
+++ src/gendiff.js
@@ -6,8 +6,6 @@ export default (path1, path2) => {
   const json1 = JSON.parse(fs.readFileSync(path.resolve(path1), 'utf8'));
   const json2 = JSON.parse(fs.readFileSync(path.resolve(path2), 'utf8'));
 
-  const result = [];
-
   const isThere = (key) => {
     if (_.has(json1, key) && !_.has(json2, key)) return `  - ${key}: ${json1[key]}`;
     if (!_.has(json1, key) && _.has(json2, key)) return `  + ${key}: ${json2[key]}`;
@@ -20,9 +18,7 @@ export default (path1, path2) => {
   const mergedJson = { ...json1, ...json2 };
   const keys = Object.keys(mergedJson).sort();
 
-  for (const key of keys) {
-     result.push(isThere(key));
-   }
+  const result = keys.map((key) => isThere(key));
 
   const res = result.join('\n');
   return `{\n${res}\n}`;
